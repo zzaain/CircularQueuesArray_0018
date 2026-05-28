470107af06cf3bbc88d3ddadebbbe5275d2cf0e5
zzaain <sheriellaghzain@gmail.com>
2026-05-28 15:04:30 +0700

    buat class queues

---
+++ queue.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+class Queues
+{
+private:
+    static const int max = 3;
+    int FRONT, REAR;
+    int queue_array[max];
+};
+
+int main()
+{
+    return 0;
+}
