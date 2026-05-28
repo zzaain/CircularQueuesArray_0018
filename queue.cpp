85ce40a060ad59e7c07f88c08b9fd48abc922aa3
zzaain <sheriellaghzain@gmail.com>
2026-05-28 15:11:26 +0700

    nambah logika insert data dan circular queue

--- queue.cpp
+++ queue.cpp
@@ -28,6 +28,20 @@ public:
             cout << "\nQueue overflow\n";
             return;
         }
+     
+    if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else
+        {
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
     }
 };
 
