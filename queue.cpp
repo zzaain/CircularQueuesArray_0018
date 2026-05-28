0966d7bb033f6043caba55f33e63c1b01fd7dd90
zzaain <sheriellaghzain@gmail.com>
2026-05-28 15:17:50 +0700

    nambah try catch di dalam looping main

--- queue.cpp
+++ queue.cpp
@@ -114,41 +114,48 @@ int main()
 
     while (true)
     {
-        cout << "Menu" << endl;
-        cout << "1. Implement insert operation" << endl;
-        cout << "2. Implement Delete Operation" << endl;
-        cout << "3. Display values " << endl;
-        cout << "4. Exit " << endl;
-        cout << "Enter your choice (1-4): ";
-        cin >> ch;
-        cout << endl;
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. Implement insert operation" << endl;
+            cout << "2. Implement Delete Operation" << endl;
+            cout << "3. Display values " << endl;
+            cout << "4. Exit " << endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
 
-        switch (ch)
-        {
-        case '1':
-        {
-            q.insert();
-            break;
+            switch (ch)
+            {
+            case '1':
+            {
+                q.insert();
+                break;
+            }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
+            default:
+            {
+                cout << "Invalid option!!!" << endl;
+                break;
+            }
+            }
         }
-        case '2':
+        catch (exception &e)
         {
-            q.remove();
-            break;
-        }
-        case '3':
-        {
-            q.display();
-            break;
-        }
-        case '4':
-        {
-            return 0;
-        }
-        default:
-        {
-            cout << "Invalid option!!!" << endl;
-            break;
-        }
+            cout << "check for the values entered." << endl;
         }
     }
     return 0;
