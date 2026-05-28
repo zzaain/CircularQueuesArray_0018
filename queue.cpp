b3da55d404f29081a21fe62588086b2a9c37d647
zzaain <sheriellaghzain@gmail.com>
2026-05-28 15:16:26 +0700

    nambah logika cetak data kondisi looping circular di fungsi display

--- queue.cpp
+++ queue.cpp
@@ -87,6 +87,23 @@ public:
             }
             cout << endl;
         }
+        else
+        {
+            while (FRONT_Position <= max - 1)
+            {
+                cout << queue_array[FRONT_Position] << "  ";
+                FRONT_Position++;
+            }
+
+            FRONT_Position = 0;
+
+            while (FRONT_Position <= REAR_Position)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+            cout << endl;
+        }
     }
 };
 
