7e42c490ad0855253f7469c16190beff0b8ac05f
zzaain <sheriellaghzain@gmail.com>
2026-05-28 15:09:17 +0700

    nambah cek queue overflow di fungsi insert

--- queue.cpp
+++ queue.cpp
@@ -22,6 +22,12 @@ public:
         cout << "Enter a number: ";
         cin >> num;
         cout << endl;
+
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
+        }
     }
 };
 
