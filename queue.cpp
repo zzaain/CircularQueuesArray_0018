d40af51290c6a164797ab03c2f800300a86e94cd
zzaain <sheriellaghzain@gmail.com>
2026-05-28 15:15:34 +0700

    nambah fungsi display dan cek queue kosong

--- queue.cpp
+++ queue.cpp
@@ -65,6 +65,19 @@ public:
                 FRONT = FRONT + 1;
         }
     }
+
+    void display()
+    {
+        int FRONT_Position = FRONT;
+        int REAR_Position = REAR;
+
+        if (FRONT_Position == -1)
+        {
+            cout << "Queue is empty\n";
+            return;
+        }
+        cout << "\nElements in the queue are....\n";
+    }
 };
 
 int main()
