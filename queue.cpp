2669a2720e8989c8df66e2dc7c0b7d60e3c1e01e
zzaain <sheriellaghzain@gmail.com>
2026-05-28 15:06:57 +0700

    nambah constructor queue

--- queue.cpp
+++ queue.cpp
@@ -7,9 +7,19 @@ private:
     static const int max = 3;
     int FRONT, REAR;
     int queue_array[max];
+
+
+public:
+    Queues()
+    {
+        FRONT = -1;
+        REAR = -1;
+    }
 };
 
 int main()
-{
+{   
+    Queues q;
     return 0;
+    
 }
