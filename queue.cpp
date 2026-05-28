857f9cee481fedee13ea35a7a19c23f948778afa
zzaain <sheriellaghzain@gmail.com>
2026-05-28 15:17:26 +0700

    nambah switch case untuk handle pilihan menu di main

--- queue.cpp
+++ queue.cpp
@@ -122,6 +122,34 @@ int main()
         cout << "Enter your choice (1-4): ";
         cin >> ch;
         cout << endl;
+
+        switch (ch)
+        {
+        case '1':
+        {
+            q.insert();
+            break;
+        }
+        case '2':
+        {
+            q.remove();
+            break;
+        }
+        case '3':
+        {
+            q.display();
+            break;
+        }
+        case '4':
+        {
+            return 0;
+        }
+        default:
+        {
+            cout << "Invalid option!!!" << endl;
+            break;
+        }
+        }
     }
     return 0;
 }
