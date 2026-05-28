e4801205414ac6701422bcc3cc12b5a37a72122f
zzaain <sheriellaghzain@gmail.com>
2026-05-28 15:16:53 +0700

    nambah loop menu interaktif di fungsi main

--- queue.cpp
+++ queue.cpp
@@ -110,5 +110,18 @@ public:
 int main()
 {
     Queues q;
+    char ch;
+
+    while (true)
+    {
+        cout << "Menu" << endl;
+        cout << "1. Implement insert operation" << endl;
+        cout << "2. Implement Delete Operation" << endl;
+        cout << "3. Display values " << endl;
+        cout << "4. Exit " << endl;
+        cout << "Enter your choice (1-4): ";
+        cin >> ch;
+        cout << endl;
+    }
     return 0;
 }
